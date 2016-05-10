b1d42eeca9d5bded3f9ac69efe4737032c595c35
Anthony Perritano <aperritano@gmail.com>
2016-05-09 22:15:00 -0500

    minor change

--- BeaconTerminal/AppDelegate.swift
+++ BeaconTerminal/AppDelegate.swift
@@ -22,10 +22,10 @@ let LOG: XCGLogger = {
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     var realm: Realm?
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         ESTConfig.setupAppID("location-configuration-07n", andAppToken: "f7532cffe8a1a28f9b1ca1345f1d647e")
         
@@ -33,19 +33,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         do {
             try NSFileManager.defaultManager().removeItemAtURL(Realm.Configuration.defaultConfiguration.fileURL!)
             
-
+            
         } catch {}
-    
+        
         
         realm = try! Realm() // Create realm pointing to default file
         
-
+        
         //createDataStructure()
-
+        
         
         LOG.debug("\(Realm.Configuration.defaultConfiguration.fileURL!)")
         
-
+        
         return true
     }
     
@@ -55,7 +55,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let path = NSBundle.mainBundle().pathForResource("wallcology_configuration", ofType: "json")
             let jsonData = NSData(contentsOfFile:path!)
             let json = JSON(data: jsonData!)
-
+            
             let configuration = Configutation()
             
             if let habitats = json["habitats"].array {
@@ -121,16 +121,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     }
                     
                 }
-
+                
             }
             try! realm!.write {
                 realm!.add(configuration)
             }
             
             
-   
+            
         } catch {
-//            print("error")
+            //            print("error")
+            LOG.debug("error")
+            //            print("error")
             LOG.debug("error")
         }
         
@@ -152,29 +154,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func createTestHabitat() {
         
     }
-
-
+    
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
